84fc07b108f68528cbe132927b2eaa0f3ba84908
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-02-22 16:29:32 -0500

    debut de script

---
+++ index.js
@@ -0,0 +1,7 @@
+//Declaration et initialisation des variables
+let password = document.getElementById("passwordInput");
+let copyPassword = document.getElementById("copyIcon");
+let btn = document.getElementById("genePassword-btn");
+
+//Creons une fonction permettant de generer aleatoirement les mots de passe
+function generatePassword() {}
