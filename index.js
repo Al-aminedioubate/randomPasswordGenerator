9aaa437971fcb6d1d130a412bd7b8af2b6cfa269
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-02-22 17:07:42 -0500

    generation du password en cours

--- index.js
+++ index.js
@@ -3,5 +3,18 @@ let password = document.getElementById("passwordInput");
 let copyPassword = document.getElementById("copyIcon");
 let btn = document.getElementById("genePassword-btn");
 
+const Characters = {
+	Uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	Lowercase: "abcdefghijklmnopqrstuvwxyz",
+	Numbers: "0123456789",
+	Symbols: "~!@#$%^&*()_+{}[].,:;|",
+};
+
 //Creons une fonction permettant de generer aleatoirement les mots de passe
-function generatePassword() {}
+function generatePassword() {
+	let randomUpperCase = Math.floor(Math.random() * Characters);
+
+	console.log(randomUpperCase);
+}
+
+generatePassword();
